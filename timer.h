968bd0ce519e92109fa785f179b0825b412cbe78
irongamer54 <vladfed0675@gmail.com>
2023-03-29 17:40:19 +0300

    Добавлен класс Timer для вынесения таймера Улучшение читаемости кода

---
+++ timer.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Arduino.h>
+class Timer {
+  public:
+    Timer(uint16_t nprd = 0) {
+      setPeriod(nprd);
+    }
+    void setPeriod(uint16_t nprd) {
+      prd = nprd;
+    }
+    uint16_t getPeriod() {
+      return prd;
+    }
+    bool ready() {
+      if (millis() - tmr >= prd) {
+        tmr = millis();
+        return true;
+      }
+      return false;
+    }
+  private:
+    uint32_t tmr = 0;
+    uint16_t prd = 0;
+};
