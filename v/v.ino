e97c20b5ca720c9686172d5897d7265df054c938
ValerjjanKa <valeria.nepomilueva255@gmail.com>
2023-04-18 15:52:18 +0500

    Скорость камеры
    ААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААА

---
+++ v/v.ino
@@ -0,0 +1,40 @@
+#define M A3
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(M, INPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint32_t t1 = millis();
+
+  int mgn = digitalRead(M);
+  while (mgn == 1) {
+    mgn = digitalRead(M);
+  }
+  
+
+  uint32_t t2 = millis();
+  double v;
+  if (t2-t1!=0) {
+   v = 2/((t2-t1)*0.001);
+
+  }
+  
+  /*Serial.print("millis =");
+  Serial.println(millis());
+
+  Serial.print("mgn = ");
+  Serial.println(mgn);
+  */
+  
+  Serial.print("v = ");
+  Serial.println(v);
+
+  
+  delay(10);
+
+}
