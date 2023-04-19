0574b0d27add8173cda912e23c21a78bf0f7f78e
irongamer54 <vladfed0675@gmail.com>
2023-04-19 12:51:52 +0500

    Начало приписания основного кода

--- config.h
+++ config.h
@@ -15,10 +15,28 @@
 
 #define MAGN_PIN A3 //пин датчика холла
 #define COUNT_MAG 1 //количество магнитов
+
 ////////********     Интервалы для таймеров    ********////////
 #define DS_UPDATE_TIME 2000
 #define ADS_UPDATE_TIME 2000
+#define SEND_DATA_DELAY 1000
 
 
 #define COUNT_FLTR 10   //количество фильтраций
 #define SERIAL_SPEED 115200 
+
+////////********     Начальные значения    ********////////
+
+#define START_ANGL_1 0
+#define START_ANGL_2 0
+#define START_SPEED 0
+
+////////********     Начальные значения    ********////////
+
+#define EXP_START_ALTITUDE 15000 // высота начала эксперенмента 
+#define EXP_START_TIME  //время начала эксперемента (Поставить верное в секундах)
+
+#define SAFE_START_ALTITUDE 2000 // Режим для защиты от внешних факторов
+#define SAFE_START_TIME 3600 // 
+
+#define MAX_EL_TEMP 70 //максимальная температура электроники 
